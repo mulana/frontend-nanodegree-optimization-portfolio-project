e6fcb91002fc55f09a4cc312554dc87959350d3a
mulana <mulana82@gmail.com>
2017-03-30 15:30:14 -0700

    chore: Add responsive_images task to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
-
+  // Load all plugin that provides.
+  require('load-grunt-tasks')(grunt);
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -8,13 +9,38 @@ module.exports = function(grunt) {
         src: 'js/perfmatters.js',
         dest: 'js/perfmatters.min.js'
       }
+    },
+    responsive_images: {
+      dev: {
+        options: {
+          sizes: [{
+            name: 'small',
+            width: 320,
+            height: 240
+            },{
+            name: 'medium',
+            width: 500
+            },{
+            name: 'large',
+            width: 800
+            },{
+            name: "large",
+            width: 1600,
+            separator: "-",
+            suffix: "_x2",
+            quality: 50
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{jpg,gif,png}'],
+          cwd: 'img/',
+          dest: 'src/img'
+        }]
+      }
     }
   });
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'responsive_images']);
 
 };
